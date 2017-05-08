f9c879a589457212f19681e428106232a771a78e
Lauren Holloway <laurenholloway1010@gmail.com>
2017-05-08 16:30:39 -0400

    AK added

--- jqueryjsonapis/Scripts/myjQuery.js
+++ jqueryjsonapis/Scripts/myjQuery.js
@@ -1,1 +1,32 @@
-﻿
+﻿$(function () {
+    $('#viewAllButton').on('click', function () {
+        var $listItems = $("li");
+        $listItems.css("display", "block");
+    }
+    )
+})
+
+$(function () {
+    $('#findButton').on('click', function () {
+        //get search term from input box
+        var $searchTerm = $("#veggieInput").val();
+
+
+        ////variable for all list items (recipes)
+        var $listItems = $("li");
+
+        $listItems.each(function () {
+            if ($(this).text().includes($searchTerm)) {
+                $(this).css("display", "block");
+            }
+            else {
+                $(this).css("display", "none");
+            }
+
+
+        })
+
+    })
+
+})
+
